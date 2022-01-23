5f4c941a2bfa4e2e6a394f02a2ed249fe1fc0b8b
Ramiro Vignolo <ramiro.vignolo@juliustech.co>
2022-01-23 17:10:08 -0300

    + async plots

--- demo/pincell.jl
+++ demo/pincell.jl
@@ -3,7 +3,7 @@ using RayTracing
 using Gridap
 
 # load geometry
-jsonfile = joinpath(@__DIR__,"pincell.json")
+jsonfile = joinpath(@__DIR__, "pincell.json")
 model = DiscreteModelFromFile(jsonfile)
 
 # number of azimuthal angles
@@ -52,8 +52,8 @@ segmentize!(tg)
 using GLMakie
 using DataStructures: CircularBuffer
 
-nφ = 8
-δ = 0.1
+nφ = 16
+δ = 0.08
 bc = Reflective
 bcs = BoundaryConditions(top=bc, bottom=bc, left=bc, right=bc)
 tg = TrackGenerator(model, nφ, δ, bcs=bcs)
@@ -64,18 +64,6 @@ fig = Figure(); display(fig)
 ax = Axis(fig[1,1])
 GLMakie.xlims!(ax, (tg.mesh.bbmin.x, tg.mesh.bbmax.x))
 GLMakie.ylims!(ax, (tg.mesh.bbmin.y, tg.mesh.bbmax.y))
-
-track = first(tg.tracks_by_uid)
-x1, y1 = track.p
-tail = 50_000
-traj = CircularBuffer{Point2f}(tail)
-fill!(traj, Point2f(x1, y1))
-traj = Observable(traj)
-
-c = to_color(:black);
-tailcol = [RGBAf(c.r, c.g, c.b, (i/tail)^2) for i in 1:tail];
-lines!(ax, traj; linewidth = 1, color = tailcol)
-# lines!(ax, traj; linestyle = :dot, linewidth = 3, color = tailcol)
 
 function animstep!(segment, dir, traj)
     if dir == RayTracing.Forward
@@ -93,29 +81,112 @@ end
 
 # esta es una funcion que plotea una trajectoria que es cyclica. creo que deberia lanzar una
 # trajectoria por n_track_x y n_track_y
-track = first(tg.tracks_by_uid)
-dir = RayTracing.Forward
-while(track !== nothing)
+function trajectory(ax, initial_track, initial_dir)
 
-    segments = dir == RayTracing.Backward ? reverse(track.segments) : track.segments
-    for segment in segments
-        animstep!(segment, dir, traj)
-        # sleep(0.02)
-        sleep(0.001)
-    end
+    track = initial_track
+    dir = initial_dir
 
-    # to separate tracks in segments
-    push!(traj[], Point2f(NaN, NaN))
+    x1, y1 = track.p
+    tail = 10_000
+    traj = CircularBuffer{Point2f}(tail)
+    fill!(traj, Point2f(x1, y1))
+    traj = Observable(traj)
 
-    # @show dir
-    # @show track.track_idx
+    c = to_color(:black);
+    tailcol = [RGBAf(c.r, c.g, c.b, (i/tail)^2) for i in 1:tail];
+    lines!(ax, traj; linewidth = 1, color = tailcol)
+    # lines!(ax, traj; linestyle = :dot, linewidth = 3, color = tailcol)
 
-    if dir == RayTracing.Forward
-        dir = RayTracing.dir_next_track_fwd(track)
-        track = track.next_track_fwd
-    else
-        dir = RayTracing.dir_next_track_bwd(track)
-        track = track.next_track_bwd
+    while(true)
+
+        segments = dir == RayTracing.Backward ? reverse(track.segments) : track.segments
+        for segment in segments
+            animstep!(segment, dir, traj)
+            sleep(0.005)
+        end
+
+        # to separate tracks in segments
+        push!(traj[], Point2f(NaN, NaN))
+
+        # @show dir
+        # @show track.track_idx
+
+        if dir == RayTracing.Forward
+            dir = RayTracing.dir_next_track_fwd(track)
+            track = track.next_track_fwd
+        else
+            dir = RayTracing.dir_next_track_bwd(track)
+            track = track.next_track_bwd
+        end
+
+        track.uid != initial_track.uid || break
     end
 
 end
+
+# trajectory(ax, first(tg.tracks_by_uid), RayTracing.Forward)
+
+# elegir varios tracks que tengan diferentes azimuthal
+for azimutal_tracks in tg.tracks
+    # for track in (tg.tracks[1][1], tg.tracks[2][1], tg.tracks[3][1])
+    @async trajectory(ax, azimutal_tracks[1], RayTracing.Forward)
+end
+
+
+# record(fig, "video.mp4", frames; framerate = 60) do i # i = frame number
+#     for j in 1:5 # step 5 times per frame
+#         animstep!(integ, rod, balls, traj)
+#     end
+#     # any other manipulation of the figure here...
+# end
+
+
+
+# mesh plot
+# @recipe function plot(mesh::Mesh)
+    mesh1 = tg.mesh
+    using UnPack
+    @unpack cell_nodes, model = mesh1
+
+    grid = get_grid(model)
+    nodes = RayTracing.get_node_coordinates(grid)
+
+    # number of cells and number of nodes per cell
+    nc = length(cell_nodes)
+    nn = all(l -> isequal(l, length(cell_nodes[1])), length.(cell_nodes)) ? length(cell_nodes[1]) : error("error")
+
+    x = Vector{Float64}(undef, (nn + 1) * nc + nc + nc) # TODO: compute Float64
+    y = Vector{Float64}(undef, (nn + 1) * nc + nc + nc)
+
+    k = 1
+    for (i, nodes_ids) in enumerate(cell_nodes)
+
+        for (j, node_id) in enumerate(nodes_ids)
+
+            x[k] = nodes[node_id][1]
+            y[k] = nodes[node_id][2]
+
+            k += 1
+        end
+
+        x[k] = nodes[nodes_ids[1]][1]
+        y[k] = nodes[nodes_ids[1]][2]
+
+        k += 1
+
+        x[k] = NaN
+        y[k] = NaN
+
+        k += 1
+    end
+
+    lines!(ax, x, y, linewidth = 0.3, color = :black)
+
+#     linecolor   --> :black
+#     seriestype  :=  :path
+#     linewidth   --> 0.2
+#     legend      --> false
+#     framestyle  :=  :none
+
+#     return (x, y)
+# end
